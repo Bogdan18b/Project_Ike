d5fc86ce66ab8859146282bdf7e22244d9dbf691
bogdan <bogdanbobletec@gmail.com>
2018-07-11 07:16:36 -0400

    add login and signup links to homepage

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import { AuthRoute } from '../util/route_util';
 
+
 const App = () => (
   <div>
     <h1>IKE! by Bogdan</h1>
+    <NavLink to="/login">Log In</NavLink>
+    <NavLink to="/login">Sign Up</NavLink>
+
     <AuthRoute path="/login" component={LoginFormContainer} />
     <AuthRoute path="/signup" component={SignupFormContainer} />
   </div>
