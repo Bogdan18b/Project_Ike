cb07b54de40824fa05983ad264d45996bde0b29a
bogdan <bogdanbobletec@gmail.com>
2018-09-11 20:43:16 -0400

    refactor footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -4,16 +4,16 @@ export const Footer = () => {
   return (
     <footer className="ike-footer-main">
       <ul className="ike-footer-list">
-        <li>About</li>
+        <li>About IKE</li>
         <li><a href="https://github.com/Bogdan18b/Project_Zebra">Github</a></li>
         <li><a href="https://www.aimforawesome.com/hawaii/hawaiian-philosophy-of-life-part-1-the-7-principles-of-huna/">IKE origin</a></li>
       </ul>
       <div className="ike-footer-image"></div>
       <ul className="ike-footer-list">
-        <li>Discover</li>
+        <li>About the Author</li>
+        <li><a href="http://bogdanbobletec.us">Personal site</a></li>
         <li><a href="https://github.com/Bogdan18b">Github</a></li>
         <li><a href="https://www.linkedin.com/in/bogdanbobletec/">Linkedin</a></li>
-        <li><a href="https://angel.co/bogdan-bobletec">AngelList</a></li>
       </ul>
     </footer>
   );
