88e5c48e054e79f1555ee8d474612a51c2628d92
bogdan <bogdanbobletec@gmail.com>
2018-09-17 12:19:28 -0400

    add link to profile page

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Greeting = ({ currentUser, logout, formClass, loginClass, signupClass }) => {
+  let name;
+  name = currentUser ? currentUser.firstName : "";
   const sessionLinks = () => (
 
     <nav className={formClass}>
@@ -16,17 +18,21 @@ const Greeting = ({ currentUser, logout, formClass, loginClass, signupClass }) =
 
     </nav>
   );
-  const personalGreeting = () => {
+  const personalGreeting = (name) => {
     return (
-      <nav className={formClass}>
-        <button className={loginClass}
-          onClick={logout}>Log Out
-        </button>
-      </nav>
+      <h1 className="ike-welcome-user"
+        onClick={() =>{document.getElementsByClassName('ike-logout-list')[0].classList.toggle('hide')}}>
+        Hello, {name.toUpperCase()}!
+        <ul className="ike-logout-list hide">
+          <li><Link to="/user_details">My Profile</Link></li>
+          <li onClick={logout}>Log Out
+          </li>
+        </ul>
+      </h1>
     );
   }
 
-  return currentUser ? personalGreeting() : sessionLinks();
+  return currentUser ? personalGreeting(name) : sessionLinks();
 };
 
 
