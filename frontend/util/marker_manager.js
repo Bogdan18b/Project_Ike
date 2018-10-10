f4ebb9dc6b13c3beef2af4973a7ed40802fd6123
bogdan <bogdanbobletec@gmail.com>
2018-10-09 22:33:03 -0400

    add and style info window

--- frontend/util/marker_manager.js
+++ frontend/util/marker_manager.js
@@ -9,10 +9,24 @@ class MarkerManager {
   updateMarkers(businesses) {
     businesses.forEach((business, idx) => {
       let myLatLong = new google.maps.LatLng(business.latitude, business.longitude);
+      let contentString = '<div id="content">'+
+            `<div><a href="/#/businesses/${business.id}"> ${business.name}</a>`+
+            `<p>${business.address}</p>`+
+            '</div>'+
+            '<div>'+
+            `<img src=${business.photoURL} alt="pic"/>`+
+            '</div>'+
+            '</div>';
+        let infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
       let marker = new google.maps.Marker({
         position: myLatLong,
         title: business.name,
       });
+      marker.addListener('click', function() {
+          infowindow.open(this.map, marker);
+        });
       this.markers[business.id] = marker;
       marker.setLabel(`${idx + 1}`);
       marker.setMap(this.map);
