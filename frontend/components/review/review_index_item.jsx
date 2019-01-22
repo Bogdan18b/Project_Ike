b695fb08ebecf70be90f5c3892e3c6a38bc9ccda
bogdan <bogdanbobletec@gmail.com>
2019-01-22 13:41:42 -0500

    update reviews on home page

--- frontend/components/review/review_index_item.jsx
+++ frontend/components/review/review_index_item.jsx
@@ -3,14 +3,21 @@ import StarRating from '../star_rating';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+const showMore = e => {
+  e.target.parentElement.style.overflow = 'scroll';
+  console.log(e.target.innerHTML = "");
+}
+
 const ReviewIndexItem = ({ review }) => {
+  const len = review.body.length;
+  const show = len > 100;
   let date = review.createdAt.slice(0, 10).split("-").join("/") || "";
   return (
     <ul className="ike-review-main-list">
       <li>{review.userName}</li>
       <li><Link to={`/businesses/${review.businessId}`}>{review.businessName}</Link></li>
-      <li><StarRating rating={review.rating} /> {date}</li>
-      <li>{review.body}</li>
+      <li><StarRating rating={review.rating} /> {date}</li>   
+      <li>{ show && <span id="show-more"onClick={showMore}>+</span>}{review.body}</li>
     </ul>
   );
 };
