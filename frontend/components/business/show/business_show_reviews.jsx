495fe9ee222386d1d8f74861d37a0896767d4839
bogdan <bogdanbobletec@gmail.com>
2019-06-21 21:38:33 -0400

    convert reviews on business show page to hooks

--- frontend/components/business/show/business_show_reviews.jsx
+++ frontend/components/business/show/business_show_reviews.jsx
@@ -1,61 +1,46 @@
-import React, { Component } from 'react';
+import React, { Fragment, useState } from 'react';
 import StarRating from '../../star_rating';
-import { withRouter } from "react-router";
 import PropTypes from 'prop-types';
 
-  class Reviews extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        reviews: this.props.reviews,
-        className: "hide"
-      };
-      this.handleHover = this.handleHover.bind(this);
-      this.handleHoverOut = this.handleHoverOut.bind(this);
-    }
+const Reviews = props => {
+  const [reviews, setReviews] = useState(props.reviews);
+  const [hideClass, setHideClass] = useState('hide');
 
-    handleRemove(id) {
-      this.props.deleteReview(id);
-      let copy = this.props.reviews.filter(review => review.id !== id);
-      this.setState({reviews: copy});
-      document.location.reload();
-    }
-
-    handleHover() {
-      this.setState({className: ""});
-    }
-
-    handleHoverOut() {
-      this.setState({className: "hide"});
-    }
-
-    render() {
-      const rev = this.state.reviews.map(review => {
+  const handleRemove = id => {
+    props.deleteReview(id);
+    const remainingReviews = props.reviews.filter(review => review.id !== id);
+    setReviews(remainingReviews);
+  }
+  const handleHover = () => setHideClass('');
+  const handleHoverOut = () => setHideClass('hide');
+  return (
+    <Fragment>
+      {reviews.map(review => {
+        const showRemoveButton = props.currentUserId === review.userId;
         return (
           <ul key={review.id}>
-            <li>{this.props.users[review.userId].firstName}</li>
-            <li><StarRating rating={review.rating}/>
-              {review.createdAt.slice(0, 10).split("-").join("/") || ""}</li>
+            <li>{props.users[review.userId].firstName}</li>
+            <li><StarRating rating={review.rating} />
+              {review.createdAt.slice(0, 10).split("-").join("/")}</li>
             <li>{review.body}</li>
-            {this.props.currentUserId === review.userId ? <li id="ike-remove-review"onClick={() => this.handleRemove(review.id)}><i className="fas fa-trash-alt"
-            onMouseOver={this.handleHover.bind()}
-            onMouseOut={this.handleHoverOut.bind()}></i>
-            <p className={`ike-business-remove-review ${this.state.className}`}>Remove Review</p></li> : ""}
+            {showRemoveButton && <li id="ike-remove-review" onClick={() => handleRemove(review.id)}><i className="fas fa-trash-alt"
+              onMouseOver={handleHover}
+              onMouseOut={handleHoverOut}
+            ></i>
+              <p className={`ike-business-remove-review ${hideClass}`}>Remove Review</p></li>}
           </ul>
-        )}
-      );
-      return (
-        rev
-      );
-    }
-  }
+        )
+      })}
+    </Fragment>
+  )
+}
 
-  Reviews.propTypes = {
-    businessId: PropTypes.number,
-    currentUserId: PropTypes.number,
-    deleteReview: PropTypes.func,
-    reviews: PropTypes.arrayOf(PropTypes.object),
-    users: PropTypes.object
-  };
+Reviews.propTypes = {
+  businessId: PropTypes.number,
+  currentUserId: PropTypes.number,
+  deleteReview: PropTypes.func,
+  reviews: PropTypes.arrayOf(PropTypes.object),
+  users: PropTypes.object
+};
 
-  export default withRouter(Reviews);
+export default Reviews;
