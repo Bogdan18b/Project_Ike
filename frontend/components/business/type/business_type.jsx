ebd6490fe18a768f6f838e3504b6e14377c0062e
bogdan <bogdanbobletec@gmail.com>
2019-06-29 08:39:47 -0400

    remove index used for icon height

--- frontend/components/business/type/business_type.jsx
+++ frontend/components/business/type/business_type.jsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import  BusinessTypeIndexItem from './business_type_index_item';
-import PropTypes from 'prop-types';
+import React from "react";
+import BusinessTypeIndexItem from "./business_type_index_item";
+import PropTypes from "prop-types";
 
 const BusinessType = ({ types, receiveTypeFromSearch }) => {
-  let classType = (window.location.href.includes("businesses") ||
-    window.location.href.includes("user_details")) ?
-    "" : "-home";
+  let classType =
+    window.location.href.includes("businesses") ||
+    window.location.href.includes("user_details")
+      ? ""
+      : "-home";
   if (types === undefined) {
     return null;
   }
-  const typ = types.map((type, idx) =>
-    <BusinessTypeIndexItem key={type.id}
+  const typ = types.map((type, idx) => (
+    <BusinessTypeIndexItem
+      key={type.id}
       receiveTypeFromSearch={receiveTypeFromSearch}
-      type={type} y={idx}/>);
-  return (
-    <ul className={`ike-business-header-nav${classType}`}>
-      {typ}
-    </ul>
-  )
-}
+      type={type}
+    />
+  ));
+  return <ul className={`ike-business-header-nav${classType}`}>{typ}</ul>;
+};
 
 BusinessType.propTypes = {
   types: PropTypes.arrayOf(PropTypes.object),
   receiveTypeFromSearch: PropTypes.func
-}
+};
 
 export default BusinessType;
--- frontend/components/business/type/business_type_index_item.jsx
+++ frontend/components/business/type/business_type_index_item.jsx
@@ -1,22 +1,19 @@
-import React from 'react';
-import { withRouter } from 'react-router';
-import PropTypes from 'prop-types';
+import React from "react";
+import { withRouter } from "react-router";
+import PropTypes from "prop-types";
 
-const BusinessTypeIndexItem = props => {
-    let y = (props.y);
-    let x = -28 + (-28 * y);
-    return (
-      <li onClick={() => {
-          props.receiveTypeFromSearch(props.type);
-          props.history.push(`/businesses/search?query=${props.type.name}`);
-        }}
-        style={{backgroundPositionY: `${x}px`}}
-        key={`type-${props.type.id}`}>{props.type.name}</li>
-    );
-}
+const BusinessTypeIndexItem = props => (
+  <li
+    onClick={() => {
+      props.receiveTypeFromSearch(props.type);
+      props.history.push(`/businesses/search?query=${props.type.name}`);
+    }}
+  >
+    {props.type.name}
+  </li>
+);
 
 BusinessTypeIndexItem.propTypes = {
-  y: PropTypes.number,
   receiveTypeFromSearch: PropTypes.func,
   type: PropTypes.shape({
     id: PropTypes.number,
