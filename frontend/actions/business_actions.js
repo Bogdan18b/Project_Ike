4aa6e85719ef3a240776a1fcf07365a5ea7c4770
bogdan <bogdanbobletec@gmail.com>
2019-06-29 08:37:16 -0400

    remove explicit return

--- frontend/actions/business_actions.js
+++ frontend/actions/business_actions.js
@@ -1,49 +1,34 @@
-import * as BusinessApiUtil from '../util/business_api_util';
+import * as BusinessApiUtil from "../util/business_api_util";
 
-export const FETCH_ALL_BUSINESSES = 'FETCH_ALL_BUSINESSES';
-export const FETCH_BUSINESS = 'FETCH_BUSINESS';
-export const FETCH_ALL_BUSINESS_TYPES = 'FETCH_ALL_BUSINESS_TYPES';
+export const FETCH_ALL_BUSINESSES = "FETCH_ALL_BUSINESSES";
+export const FETCH_BUSINESS = "FETCH_BUSINESS";
+export const FETCH_ALL_BUSINESS_TYPES = "FETCH_ALL_BUSINESS_TYPES";
 
-export const fetchAllBusinesses = (businesses) => {
-  return {
-    type: FETCH_ALL_BUSINESSES,
-    businesses
-  };
-};
-export const fetchBusiness = (payload) => {
-  return {
-    type: FETCH_BUSINESS,
-    payload
-  };
-};
+export const fetchAllBusinesses = businesses => ({
+  type: FETCH_ALL_BUSINESSES,
+  businesses
+});
+export const fetchBusiness = payload => ({
+  type: FETCH_BUSINESS,
+  payload
+});
 
-export const fetchAllBusinessTypes = (types) => {
-  return {
-    type: FETCH_ALL_BUSINESS_TYPES,
-    types
-  };
-};
+export const fetchAllBusinessTypes = types => ({
+  type: FETCH_ALL_BUSINESS_TYPES,
+  types
+});
 
-export const requestAllBusinesses = () => {
-  return dispatch => {
-    return BusinessApiUtil.fetchAllBusinesses().then(businesses => {
-      return dispatch(fetchAllBusinesses(businesses));
-    });
-  };
-};
+export const requestAllBusinesses = () => dispatch =>
+  BusinessApiUtil.fetchAllBusinesses().then(businesses =>
+    dispatch(fetchAllBusinesses(businesses))
+  );
 
-export const requestBusiness = (id) => {
-  return dispatch => {
-    return BusinessApiUtil.fetchBusiness(id).then((payload) => {
-      return dispatch(fetchBusiness(payload));
-    });
-  };
-};
+export const requestBusiness = id => dispatch =>
+  BusinessApiUtil.fetchBusiness(id).then(payload =>
+    dispatch(fetchBusiness(payload))
+  );
 
-export const requestAllBusinessTypes = () => {
-  return dispatch => {
-    return BusinessApiUtil.fetchAllBusinessTypes().then(types => {
-      return dispatch(fetchAllBusinessTypes(types));
-    });
-  };
-};
+export const requestAllBusinessTypes = () => dispatch =>
+  BusinessApiUtil.fetchAllBusinessTypes().then(types =>
+    dispatch(fetchAllBusinessTypes(types))
+  );
