2bbed4d4386454313b734d5ac7b0c364e5bff767
bogdan <bogdanbobletec@gmail.com>
2019-06-29 08:38:27 -0400

    apply prettier

--- frontend/components/business/popular/business.js
+++ frontend/components/business/popular/business.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import StarRating from '../../star_rating';
-import { Link } from 'react-router-dom';
+import React from "react";
+import StarRating from "../../star_rating";
+import { Link } from "react-router-dom";
 
-const PopularBusiness = ({ business }) => {
-  return (
-    <ul>
-      <li><img src={business.photoURL} alt={business.name}/></li>
-      <li><Link to={`/businesses/${business.id}`}>{business.name}</Link></li>
-      <li><div><StarRating rating={business.rating} /><p>{business.reviewIds.length} review{business.reviewIds.length === 1 ? "" : "s"}</p></div></li>
-      <li><p>{business.tags.join(", ")}</p></li>
-    </ul>
-  )
-};
-
+const PopularBusiness = ({ business }) => (
+  <ul>
+    <li>
+      <img src={business.photoURL} alt={business.name} />
+    </li>
+    <li>
+      <Link to={`/businesses/${business.id}`}>{business.name}</Link>
+    </li>
+    <li>
+      <div>
+        <StarRating rating={business.rating} />
+        <p>
+          {business.reviewIds.length} review
+          {business.reviewIds.length === 1 ? "" : "s"}
+        </p>
+      </div>
+    </li>
+    <li>
+      <p>{business.tags.join(", ")}</p>
+    </li>
+  </ul>
+);
 export default PopularBusiness;
