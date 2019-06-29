06183e25d6ce1727c42366b4fdaea7c3f19b6624
bogdan <bogdanbobletec@gmail.com>
2019-06-29 08:40:24 -0400

    apply prettier

--- frontend/components/business/show/business_show_reviews.jsx
+++ frontend/components/business/show/business_show_reviews.jsx
@@ -1,18 +1,18 @@
-import React, { Fragment, useState } from 'react';
-import StarRating from '../../star_rating';
-import PropTypes from 'prop-types';
+import React, { Fragment, useState } from "react";
+import StarRating from "../../star_rating";
+import PropTypes from "prop-types";
 
 const Reviews = props => {
   const [reviews, setReviews] = useState(props.reviews);
-  const [hideClass, setHideClass] = useState('hide');
+  const [hideClass, setHideClass] = useState("hide");
 
   const handleRemove = id => {
     props.deleteReview(id);
     const remainingReviews = props.reviews.filter(review => review.id !== id);
     setReviews(remainingReviews);
-  }
-  const handleHover = () => setHideClass('');
-  const handleHoverOut = () => setHideClass('hide');
+  };
+  const handleHover = () => setHideClass("");
+  const handleHoverOut = () => setHideClass("hide");
   return (
     <Fragment>
       {reviews.map(review => {
@@ -20,20 +20,35 @@ const Reviews = props => {
         return (
           <ul key={review.id}>
             <li>{props.users[review.userId].firstName}</li>
-            <li><StarRating rating={review.rating} />
-              {review.createdAt.slice(0, 10).split("-").join("/")}</li>
+            <li>
+              <StarRating rating={review.rating} />
+              {review.createdAt
+                .slice(0, 10)
+                .split("-")
+                .join("/")}
+            </li>
             <li>{review.body}</li>
-            {showRemoveButton && <li id="ike-remove-review" onClick={() => handleRemove(review.id)}><i className="fas fa-trash-alt"
-              onMouseOver={handleHover}
-              onMouseOut={handleHoverOut}
-            ></i>
-              <p className={`ike-business-remove-review ${hideClass}`}>Remove Review</p></li>}
+            {showRemoveButton && (
+              <li
+                id="ike-remove-review"
+                onClick={() => handleRemove(review.id)}
+              >
+                <i
+                  className="fas fa-trash-alt"
+                  onMouseOver={handleHover}
+                  onMouseOut={handleHoverOut}
+                />
+                <p className={`ike-business-remove-review ${hideClass}`}>
+                  Remove Review
+                </p>
+              </li>
+            )}
           </ul>
-        )
+        );
       })}
     </Fragment>
-  )
-}
+  );
+};
 
 Reviews.propTypes = {
   businessId: PropTypes.number,
