58fd848617378bbf289305a3f4ff56c0e17ca6d5
bogdan <bogdanbobletec@gmail.com>
2019-06-30 15:38:49 -0400

    remove explicit return

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React from "react";
 
-export const Footer = () => {
-  return (
-    <footer className="ike-footer-main">
-      <ul className="ike-footer-list">
-        <li>About IKE</li>
-        <li><a href="https://github.com/Bogdan18b/Project_Zebra">Github</a></li>
-        <li><a href="https://www.aimforawesome.com/hawaii/hawaiian-philosophy-of-life-part-1-the-7-principles-of-huna/">IKE origin</a></li>
-      </ul>
-      <div className="ike-footer-image"></div>
-      <ul className="ike-footer-list">
-        <li>About the Author</li>
-        <li><a href="http://bogdanbobletec.us">Portfolio site</a></li>
-        <li><a href="https://github.com/Bogdan18b">Github</a></li>
-        <li><a href="https://www.linkedin.com/in/bogdanbobletec/">Linkedin</a></li>
-      </ul>
-    </footer>
-  );
-};
+export const Footer = () => (
+  <footer className="ike-footer-main">
+    <ul className="ike-footer-list">
+      <li>About IKE</li>
+      <li>
+        <a href="https://github.com/Bogdan18b/Project_Zebra">Github</a>
+      </li>
+      <li>
+        <a href="https://www.aimforawesome.com/hawaii/hawaiian-philosophy-of-life-part-1-the-7-principles-of-huna/">
+          IKE origin
+        </a>
+      </li>
+    </ul>
+    <div className="ike-footer-image" />
+    <ul className="ike-footer-list">
+      <li>About the Author</li>
+      <li>
+        <a href="http://bogdanbobletec.us">Portfolio site</a>
+      </li>
+      <li>
+        <a href="https://github.com/Bogdan18b">Github</a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/bogdanbobletec/">Linkedin</a>
+      </li>
+    </ul>
+  </footer>
+);
