63b18ce0faac5be609261f045c64ea82fe0ced6e
bogdan <bogdanbobletec@gmail.com>
2019-07-21 09:44:52 -0400

    rename classes and update styles

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,28 +1,28 @@
 import React from "react";
 
 export const Footer = () => (
-  <footer className="ike-footer-main">
-    <ul className="ike-footer-list">
-      <li>About IKE</li>
-      <li>
+  <footer className="Footer">
+    <ul className="Footer-list">
+      <li className="Footer-list-item">About IKE</li>
+      <li className="Footer-list-item">
         <a href="https://github.com/Bogdan18b/Project_Zebra">Github</a>
       </li>
-      <li>
+      <li className="Footer-list-item">
         <a href="https://www.aimforawesome.com/hawaii/hawaiian-philosophy-of-life-part-1-the-7-principles-of-huna/">
           IKE origin
         </a>
       </li>
     </ul>
-    <div className="ike-footer-image" />
-    <ul className="ike-footer-list">
-      <li>About the Author</li>
-      <li>
+    <div className="Footer-image" />
+    <ul className="Footer-list">
+      <li className="Footer-list-item">About the Author</li>
+      <li className="Footer-list-item">
         <a href="http://bogdanbobletec.us">Portfolio site</a>
       </li>
-      <li>
+      <li className="Footer-list-item">
         <a href="https://github.com/Bogdan18b">Github</a>
       </li>
-      <li>
+      <li className="Footer-list-item">
         <a href="https://www.linkedin.com/in/bogdanbobletec/">Linkedin</a>
       </li>
     </ul>
