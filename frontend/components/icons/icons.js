f2fd730c37a8366b935e8d23a213426f30af94c7
bogdan <bogdanbobletec@gmail.com>
2019-08-09 21:56:23 -0400

    replace icons with svg

---
+++ frontend/components/icons/icons.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Dumbbell from "./dumbbell";
+import Salon from "./salon";
+import Pets from "./pets";
+import Electronics from "./electronics";
+import Tools from "./tools";
+import ForkKnife from "./forkknife";
+
+const Icons = [
+  <Dumbbell />,
+  <ForkKnife />,
+  <Salon />,
+  <Pets />,
+  <Electronics />,
+  <Tools />
+];
+
+export default Icons;
